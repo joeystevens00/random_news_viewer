dcf6e979777af9071de5edd3b23e70bb205f460c
zen <zen@localhost.localdomain>
2018-11-27 19:40:40 -0500

    rule is backwards

--- iptables.sh
+++ iptables.sh
@@ -4,4 +4,4 @@ iptables -A INPUT -i $INTERFACE -p tcp --dport 80 -j ACCEPT
 
 iptables -A INPUT -i $INTERFACE -p tcp --dport 3000 -j ACCEPT
 
-iptables -A PREROUTING -t nat -i $INTERFACE -p tcp --dport 3000 -j REDIRECT --to-port 80
+iptables -A PREROUTING -t nat -i $INTERFACE -p tcp --dport 80 -j REDIRECT --to-port 3000
